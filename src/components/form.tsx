7d46c8c0c509e423e3215584f263986903f21566
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2025-02-10 12:48:33 -0300

    chore: add motion and lucide

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -3,6 +3,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { formSchema, FormData } from "../schema";
 import { InputField } from "./input-field";
 import iconRadioSelect from "../assets/icon-radio-selected.svg";
+import iconCheck from "../assets/icon-checkbox-check.svg";
 
 export const Form = () => {
   const methods = useForm<FormData>({
@@ -28,97 +29,128 @@ export const Form = () => {
   };
 
   return (
-    <div className="w-full rounded-2xl bg-white p-4 text-[#2b4246]">
-      <h1 className="mb-7 text-2xl font-bold text-[#2b4246]">Contact US</h1>
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <fieldset className="mb-4 flex flex-col gap-y-4">
-            <InputField label="First Name" name="firstName" />
-            <InputField label="Last Name" name="lastName" />
-            <InputField label="Email Address" name="email" />
-          </fieldset>
-          {/* INPUT RADIO */}
-          <fieldset className="mb-4 flex flex-col gap-y-4">
-            <legend className="mb-2">
-              Query type <span className="text-[#0c7d69]">*</span>
-            </legend>
-            <label
-              className={`flex cursor-pointer items-center rounded border border-[#87a3a6] px-5 py-2 ${methods.watch("queryType") === "general enquity" && "bg-[#dff1e7]"}`}
-            >
-              <span
-                className={`${methods.watch("queryType") === "general enquity" && "border-0"} mr-3 block h-4 w-4 rounded-full border border-gray-300`}
-              >
-                <img
-                  src={iconRadioSelect}
-                  alt="radio selected"
-                  className={`${methods.watch("queryType") === "general enquity" ? "block" : "hidden"}`}
-                />
-              </span>
-              <input
-                type="radio"
-                value="general enquity"
-                className="hidden"
-                {...register("queryType")}
-              />
-              General Enquity
-            </label>
-            <label
-              className={`flex cursor-pointer items-center rounded border border-[#87a3a6] px-5 py-2 ${methods.watch("queryType") === "support request" && "bg-[#dff1e7]"}`}
-            >
-              <span
-                className={`${methods.watch("queryType") === "support request" && "border-0"} mr-3 block h-4 w-4 rounded-full border border-gray-300`}
-              >
-                <img
-                  src={iconRadioSelect}
-                  alt="radio selected"
-                  className={`${methods.watch("queryType") === "support request" ? "block" : "hidden"}`}
-                />
-              </span>
-              <input
-                type="radio"
-                value="support request"
-                className="hidden"
-                {...register("queryType")}
-              />
-              Support Request
-            </label>
-            {errors.queryType && (
-              <span className="-translate-y-2 text-sm text-red-500">
-                Please select a query type
-              </span>
-            )}
-          </fieldset>
-          <fieldset>
-            <label>
-              Message <span className="text-#0c7d69">*</span>
-            </label>
-            <textarea
-              className="mt-2 w-full resize-none rounded border border-[#87a3a6] px-3 py-2 outline-0"
-              rows={5}
-              {...register("message")}
-              id=""
+    <FormProvider {...methods}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-[700px] rounded-2xl bg-white p-7 text-[#2b4246]"
+      >
+        <h1 className="mb-7 text-2xl font-bold text-[#2b4246]">Contact US</h1>
+        <fieldset className="md: mb-4">
+          <div className="flex gap-x-5 space-y-3">
+            <InputField
+              label="First Name"
+              name="firstName"
+              className="w-full"
             />
-            {errors?.message && (
-              <span className="text-sm text-red-500">
-                {errors.message?.message}
-              </span>
-            )}
-          </fieldset>
-          <fieldset>
-            <label>
-              <input type="checkbox" {...register("terms")} />I consent to be
-              contacted by the team <span className="#0c7d69">*</span>
-            </label>
-            {errors.terms && <span>{errors.terms?.message}</span>}
-          </fieldset>
-          <button
-            type="submit"
-            className="mt-6 w-full rounded bg-[#0c7d69] py-2 text-white"
-          >
-            Submit
-          </button>
-        </form>
-      </FormProvider>
-    </div>
+            <InputField label="Last Name" name="lastName" />
+          </div>
+          <InputField label="Email Address" name="email" />
+        </fieldset>
+        {/* INPUT RADIO */}
+        <fieldset className="mb-4 flex flex-col gap-y-4">
+          <legend className="mb-2">
+            Query type <span className="text-[#0c7d69]">*</span>
+          </legend>
+          <div className="flex justify-between gap-x-5">
+            <div className="w-full">
+              <label
+                className={`flex cursor-pointer items-center rounded border border-[#87a3a6] px-5 py-2 ${methods.watch("queryType") === "general enquity" && "bg-[#dff1e7]"}`}
+              >
+                <span
+                  className={`${methods.watch("queryType") === "general enquity" && "border-0"} mr-3 block h-4 w-4 rounded-full border border-gray-300`}
+                >
+                  <img
+                    src={iconRadioSelect}
+                    alt="radio selected"
+                    className={`${methods.watch("queryType") === "general enquity" ? "block" : "hidden"}`}
+                  />
+                </span>
+                <input
+                  type="radio"
+                  value="general enquity"
+                  className="hidden"
+                  {...register("queryType")}
+                />
+                General Enquity
+              </label>
+            </div>
+            <div className="w-full">
+              <label
+                className={`flex cursor-pointer items-center rounded border border-[#87a3a6] px-5 py-2 ${methods.watch("queryType") === "support request" && "bg-[#dff1e7]"}`}
+              >
+                <span
+                  className={`${methods.watch("queryType") === "support request" && "border-0"} mr-3 block h-4 w-4 rounded-full border border-gray-300`}
+                >
+                  <img
+                    src={iconRadioSelect}
+                    alt="radio selected"
+                    className={`${methods.watch("queryType") === "support request" ? "block" : "hidden"}`}
+                  />
+                </span>
+                <input
+                  type="radio"
+                  value="support request"
+                  className="hidden"
+                  {...register("queryType")}
+                />
+                Support Request
+              </label>
+            </div>
+          </div>
+          {errors.queryType && (
+            <span className="-translate-y-2 text-sm text-red-500">
+              Please select a query type
+            </span>
+          )}
+        </fieldset>
+        <fieldset>
+          <label>
+            Message <span className="text-#0c7d69">*</span>
+          </label>
+          <textarea
+            className={`${errors.message && "border-red-500"} mt-2 w-full resize-none rounded border border-[#87a3a6] px-3 py-2 outline-0`}
+            rows={5}
+            {...register("message")}
+            id=""
+          />
+          {errors?.message && (
+            <span className="text-sm text-red-500">
+              {errors.message?.message}
+            </span>
+          )}
+        </fieldset>
+        <fieldset className="my-6">
+          <label className="flex cursor-pointer items-center">
+            <input
+              type="checkbox"
+              {...register("terms")}
+              className="absolute inline-block w-0"
+            />
+            {/* CHECKBOX STYLIZED */}
+            <div
+              className={`${methods.watch("terms") && "absolute border-transparent"} mr-2 h-[18px] w-[18px] border border-[#87a3a6]`}
+            />
+            <div
+              className={`${methods.watch("terms") ? "mr-2 block" : "hidden"}`}
+            >
+              <img src={iconCheck} alt="Checkbox" />
+            </div>
+            I consent to be contacted by the team{" "}
+            <span className="#0c7d69">*</span>
+          </label>
+          {errors.terms && (
+            <span className="block text-sm text-red-500">
+              {errors.terms?.message}
+            </span>
+          )}
+        </fieldset>
+        <button
+          type="submit"
+          className="w-full cursor-pointer rounded bg-[#0c7d69] py-2 text-white transition-all hover:bg-[#2b4246]"
+        >
+          Submit
+        </button>
+      </form>
+    </FormProvider>
   );
 };
